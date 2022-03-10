bb3af3d08320a2052f0c77fd0d4dd783dac94bbd
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-10 09:12:42 -0500

    Wired Up OnScreen Controls

--- GAME3004-W2022-Lab5/Assets/[Scripts]/PlayerBehaviour.cs
+++ GAME3004-W2022-Lab5/Assets/[Scripts]/PlayerBehaviour.cs
@@ -19,6 +19,12 @@ public class PlayerBehaviour : MonoBehaviour
     public LayerMask groundMask;
     public bool isGrounded;
 
+    [Header("OnScreen Controls")] 
+    public GameObject onScreenControls;
+    public GameObject miniMap;
+    public Joystick leftJoystick;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,8 +41,8 @@ public class PlayerBehaviour : MonoBehaviour
             velocity.y = -2.0f;
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal") + leftJoystick.Horizontal;
+        float z = Input.GetAxis("Vertical") + leftJoystick.Vertical;
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * maxSpeed * Time.deltaTime);
@@ -63,5 +69,18 @@ public class PlayerBehaviour : MonoBehaviour
         {
             uiController.TakeDamage(5);
         }
+    }
+
+    public void OnJumpButton_Pressed()
+    {
+        if (isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+    }
+
+    public void OnMapButton_Pressed()
+    {
+        miniMap.SetActive(!miniMap.activeInHierarchy);
     }
 }
